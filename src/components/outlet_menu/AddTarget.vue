98b5f336c2d0a2e553958b4f53aad51629689e5b
rhyanz46 <rianariansaputra@gmail.com>
2020-07-13 23:41:42 +0800

    table for list outlet on set target

--- src/components/outlet_menu/AddTarget.vue
+++ src/components/outlet_menu/AddTarget.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
-        add target outlet 
+        <div>
+            <table>
+                <tr>
+                    <th>Outlet ID</th>
+                    <th>Name</th>
+                    <th>Last Month Target</th>
+                    <th>This Mont Target</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>SUmbeeerr Cell</td>
+                    <td>100</td>
+                    <td>10</td>
+                </tr>
+            </table>
+        </div>
     </div>
 </template>
 
@@ -9,3 +24,23 @@ export default {
     name: 'AddTarget'
 }
 </script>
+
+<style scoped>
+table{
+    color: black;
+    background: white;
+    padding: 12px;
+    cursor: pointer;
+    border-spacing: 0px;
+    width: 100%;
+    border-collapse: collapse;
+}
+table th{
+    padding: 10px;
+    background: #bebdc3;
+}
+table td, th{
+    padding: 10px;
+    border: 1px solid #ddd;
+}
+</style>
