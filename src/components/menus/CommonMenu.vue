d52888aad0772988045a0f9d5ddb7c906f4a097e
rhyanz46 <rianariansaputra@gmail.com>
2020-07-13 17:50:34 +0800

    add menu

--- src/components/menus/CommonMenu.vue
+++ src/components/menus/CommonMenu.vue
@@ -4,11 +4,13 @@
             <div class="menus-container">
             </div>
             <div class="menus-container">
-                <div class="filter-list wrapper-center">Pengaturan Akun</div>
+                <div class="filter-list wrapper-center">Upload History</div>
+                <div class="filter-list wrapper-center">Data Outlet</div>
             </div>
             <div class="menus-container">
             </div>
             <div class="menus-container" >
+                <div class="filter-list wrapper-center">Pengaturan Akun</div>
                 <div class="filter-list wrapper-center" @click="logout">Keluar</div>
             </div>
             <div class="menus-container">
@@ -30,7 +32,7 @@ export default {
 </script>
 
 <style scoped>
-
+/* 
 .sub-menu{
     background: #FFFFFF;
     margin: 5px 10% 5px 10%;
@@ -58,6 +60,61 @@ export default {
 
 .filter-list{
     padding: 15px;
+    cursor: pointer;
+}
+
+.filter-list:hover{
+    background: #868686;
+    color: white;
+}
+
+.wrapper-center{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-items: stretch;
+    align-content: flex-end;
+}
+
+.menus-container {
+    position: static;
+    align-self: stretch;
+    order: 0;
+    flex-shrink: 0;
+    flex-basis: 90px;
+    padding-top: 5px;
+} */
+
+
+
+.sub-menu{
+    background: #FFFFFF;
+    margin: 5px 10% 5px 10%;
+    box-shadow: 3px 5px 19px 0px rgba(0,0,0,0.75);
+    padding: 5px;
+    border-radius: 10px;
+    height: 60px;
+}
+
+.sub-menu > .menus-container:nth-child(1) {
+    flex: 1;
+}
+
+.sub-menu > .menus-container:nth-child(2) {
+    flex: 1;
+}
+
+.sub-menu > .menus-container:nth-child(4) {
+    flex: 1;
+}
+
+.sub-menu > .menus-container:nth-child(5) {
+    flex: 1;
+}
+
+.filter-list{
+    padding: 3px;
     cursor: pointer;
 }
 
